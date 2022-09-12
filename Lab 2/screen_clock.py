12e104198e1b170e1966e4c28c09ec1018bb6260
runzeeee <runze@bu.edu>
2022-09-12 17:45:15 -0400

    new clock

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -5,6 +5,7 @@ import board
 from time import strftime, sleep
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+from adafruit_rgb_display.rgb import color565
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -66,7 +67,63 @@ while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-    draw.text((0,10), strftime("%m/%d/%Y %H:%M:%S"), font=font, fill="#FFFFFF")
+    #draw.text((0,10), strftime("%m/%d/%Y %H:%M:%S"), font=font, fill="#FFFFFF")
+    # part E
+    # Leecode year
+    buttonA = digitalio.DigitalInOut(board.D23)
+    buttonB = digitalio.DigitalInOut(board.D24)
+    buttonA.switch_to_input()
+    buttonB.switch_to_input()
+
+    Day = strftime("%d") + " Cups of Morning Coffee"
+    Month = strftime("%m") + " Times Rent Paid"
+    Year = str(int(strftime("%Y"))-2000) + " Years Old"
+    
+    def checkDayNight():
+        if int(strftime("%H")) >= 6 and int(strftime("%H")) <= 18:
+            return str(18 - int(strftime("%H")))  + " Hours Daylight Remain"
+        else:
+            if int(strftime("%H")) > 18 and int(strftime("%H")) <= 24:
+                return str(24 - int(strftime("%H")) + 6)  + " Hours Darkness Remain"
+            else:
+                return str(6 - int(strftime("%H")))  + " Hours Darkness Remaining"
+
+    def checkDaysremain():
+        thirtyone = [1,3,5,7,8,10,12]
+        thirty = [4,6,9,11]
+        if int(strftime("%d")) == 2:
+            return str(28 - int(strftime("%d")))  + " days before pay rent"
+        elif int(strftime("%d")) in thirty:
+            return str(30 - int(strftime("%m")))  + " days before pay rent"
+        elif int(strftime("%d")) in thirtyone:
+            return str(31 - int(strftime("%d")))  + " days before pay rent"
+
+    def checksectonextday():
+        return str(86400 - int(strftime("%H")) * 60 * 60 - int(strftime("%M")) * 60 - int(strftime("%S")))  + "s remain today"
+
+
+    Hour = checkDayNight()
+    MinuteSecond = str(int(strftime("%M")) * 60 + int(strftime("%S"))) + " Times Love U(Hourly)"
+
+    if buttonA.value and buttonB.value: # defalt diplay
+        #draw.text((0,10), Time, font=font, fill="#FFFFFF")
+        y = top
+        draw.text((x, y), Year, font=font, fill="#D0C1C6")
+        y += font.getsize(Year)[1]
+        draw.text((x, y), Month, font=font, fill="#69647B")
+        y += font.getsize(Month)[1]
+        draw.text((x, y), Day, font=font, fill="#92ACD1")
+        y += font.getsize(Day)[1]
+        draw.text((x, y), Hour, font=font, fill="#9FD4BE")
+        y += font.getsize(Hour)[1]
+        draw.text((x, y), MinuteSecond, font=font, fill="#F5A997")
+
+    if buttonB.value and not buttonA.value:  # just button A pressed
+        draw.text((0,10), checkDaysremain(), font=font, fill="#FFFFFF")  # set the screen to white
+    if buttonA.value and not buttonB.value:  # just button B pressed
+        draw.text((0,10), checksectonextday(), font=font, fill="#FFFFFF")  # set the screen to white
+    if not buttonA.value and not buttonB.value:  # All pressed
+        draw.text((0,10), strftime("%m/%d/%Y %H:%M:%S"), font=font, fill="#0000FF")
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
